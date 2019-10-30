30f45b41c6bd06a9398c5ef873e4b828cce1a540
Sam Forbes <samuel.h.forbes@gmail.com>
2019-10-30 16:58:44 +0000

    Data docs

---
+++ R/pupil_data.R
@@ -0,0 +1,16 @@
+
+#' Data collected in a pupillometry study by Sylvain Sirois
+#'
+#' Data from a simple study measuring pupil dilation as participants answer hard or easy maths problems.
+#' Original data sourced and reformatted from Sylvain Sirois' Pupillometry tutorial available at [here:](https://oraprdnt.uqtr.uquebec.ca/pls/public/gscw031?owa_no_site=314&owa_no_fiche=3&owa_bottin=https://oraprdnt.uqtr.uquebec.ca/pls/public/gscw031?owa_no_site=314&owa_no_fiche=3&owa_bottin=)
+#'
+#' @format A data frame with 28800 rows and 7 variables: \describe{
+#' \item{ID}{Uniaue participant ID}
+#'   \item{Trial}{Unique trial code (also unique for each participant)}
+#'   \item{RPupil}{Right pupil size}
+#'   \item{LPupil}{Left Pupil Size}
+#'   \item{Timebin}{Ordered timebin within each trial}
+#'   \item{Time}{Elapsed time within trial}
+#'   \item{Type}{Hard or easy trial?} ... }
+#' @source (https://oraprdnt.uqtr.uquebec.ca/pls/public/gscw031?owa_no_site=314&owa_no_fiche=3&owa_bottin=https://oraprdnt.uqtr.uquebec.ca/pls/public/gscw031?owa_no_site=314&owa_no_fiche=3&owa_bottin=)
+"pupil_data"
